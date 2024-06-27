ddf51010b56347c0a9ed2d4f25bad624e0b94c2e
Júlio César <juliocesar.gm6000@gmail.com>
2024-06-27 18:08:40 -0300

    finishing the section of exceptions

---
+++ udemy_poo/section11/Exercicio/Entities/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercicio.Entities.Exceptions;
+
+namespace Exercicio.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account() { }
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;  
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void Withdraw(double amount) 
+        {
+
+            if (amount > WithdrawLimit)
+                throw new WithdrawExceedsLimitException("Exception due to exceeds the limit of withdraw");
+
+            if (amount > Balance)
+                throw new NotEnoughBalanceToWithdrawException("Exception due to balance not being enough to withdraw");
+
+            Balance -= amount;
+        }
+
+    }
+}
---
+++ udemy_poo/section11/Exercicio/Program.cs
@@ -0,0 +1,37 @@
+﻿using Exercicio.Entities;
+using Exercicio.Entities.Exceptions;
+using System.Globalization;
+
+namespace Exercicio;
+public class Program { 
+ public static void Main(){
+
+    Console.WriteLine("Enter account data");
+
+    Console.Write("Number: ");
+    int Number = int.Parse(Console.ReadLine());
+    Console.Write("Holder: ");
+    string Holder = Console.ReadLine();
+    Console.Write("Initial balance: ");
+    double InitialBalance = double.Parse(Console.ReadLine());
+    Console.Write("Withdraw limit: ");
+    double WithdrawLimit = double.Parse(Console.ReadLine());
+
+    Account acc = new Account(Number, Holder, InitialBalance, WithdrawLimit);
+
+    Console.Write("\nEnter amount for withdraw: ");
+    double Amount = double.Parse(Console.ReadLine());
+
+        try
+        {
+            acc.Withdraw(Amount);
+            Console.WriteLine("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+        } catch (NotEnoughBalanceToWithdrawException ex){
+            Console.WriteLine("Withdraw error: " + ex.Message);
+        } catch(WithdrawExceedsLimitException ex)
+        {
+            Console.WriteLine("Withdraw error: " + ex.Message);
+        }
+        
+    }
+}
